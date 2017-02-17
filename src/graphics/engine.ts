10387806b5716d983cf6f3dbe4c49ceb5c6772ac
Nikhil Verma <nikhilnxvverma1@aol.com>
2017-02-16 23:19:27 -0500

    Created color shader for each vertex.

--- src/graphics/engine.ts
+++ src/graphics/engine.ts
@@ -1,8 +1,11 @@
 //Vertex Shader
 let simpleVertexShader=`
 	attribute vec4 position;
+	attribute vec4 color;
+	varying vec4 vColor;
 	void main(){
 		gl_Position=position;
+		vColor=color;
 	}
 `;
 
@@ -15,9 +18,11 @@ let simpleFragmentShader=`
 
 //Fragment Shader
 let colorFragmentShader=`
-	attribute vec4 vColor;
+	precision mediump float;
+	varying vec4 vColor;
 	void main(){
-		gl_FragColor=vec4(vColor.r,vColor.g,vColor.b,vColor.a);
+		gl_FragColor=vec4(vColor.r,vColor.g,vColor.b,1);
+		// gl_FragColor=vec4(0.4,0.3,0.8,1);
 	}
 `;
 
@@ -64,21 +69,29 @@ export class Engine{
 			console.error("webgl not available");
 			return;
 		}
+		const STEP = Float32Array.BYTES_PER_ELEMENT;
 
 		//create the two shaders and put them in a program
 		let vertexShaderId=this.createShader(this.GL.VERTEX_SHADER,simpleVertexShader);
-		let fragmentShaderId=this.createShader(this.GL.FRAGMENT_SHADER,simpleFragmentShader);
+		let fragmentShaderId=this.createShader(this.GL.FRAGMENT_SHADER,colorFragmentShader);
 
 		//put these two shaders in a list and create a program
 		let program=this.createProgram([vertexShaderId,fragmentShaderId]);
-
-		//get the location to the position attribute defined in the vertex shader
-		let positionLocation=this.GL.getAttribLocation(program,"position");
 
 		//create and bind a new buffer, and use the models to set positional attributes to it.
 		let positionBuffer=this.GL.createBuffer();
 		this.GL.bindBuffer(this.GL.ARRAY_BUFFER,positionBuffer);
 
+		//to send data to the position attribute, we must first bind to that attribute's location
+		// this.GL.bindBuffer(this.GL.ARRAY_BUFFER,positionBuffer);
+
+		//set buffer data based on the models
+		let points=[
+			0,0,0,0.5,0.1,0.9,
+			0,0.6,0,0.2,0.8,0.7,
+			0.9,0,0,0.4,0.5,0.3
+		];
+		this.GL.bufferData(this.GL.ARRAY_BUFFER,new Float32Array(points),this.GL.STATIC_DRAW);
 
 		//------Rendering------
 		this.GL.viewport(0,0,this.GL.canvas.width,this.GL.canvas.height);
@@ -90,20 +103,22 @@ export class Engine{
 		//use the shader program we setup earlier, 
 		this.GL.useProgram(program);
 
+		//get the location to the position attribute defined in the vertex shader
+		let positionLocation=this.GL.getAttribLocation(program,"position");
+
 		//enable array for position attribute of vertex shader
 		this.GL.enableVertexAttribArray(positionLocation);
 
-		//to send data to the position attribute, we must first bind to that attribute's location
-		this.GL.bindBuffer(this.GL.ARRAY_BUFFER,positionBuffer);
+		//for each vertex, identify the position in the vertex buffer so that the vertex shader could use it.
+		
+		this.GL.vertexAttribPointer(positionLocation, 3, this.GL.FLOAT, false, 6 * STEP, 0);
+		// this.GL.disableVertexAttribArray(positionLocation);
 
-		//set buffer data based on the models
-		let points=[
-			0,0,0,
-			0,0.6,0,
-			0.9,0,0
-		]
-		this.GL.bufferData(this.GL.ARRAY_BUFFER,new Float32Array(points),this.GL.STATIC_DRAW);
-		this.GL.vertexAttribPointer(positionLocation,3,this.GL.FLOAT,false,0,0);
+		//enable array for color attribute of fragment shader
+		let colorLocation = this.GL.getAttribLocation(program, "color");
+		this.GL.enableVertexAttribArray(colorLocation);
+		this.GL.vertexAttribPointer(colorLocation, 3, this.GL.FLOAT, false, 6 * STEP, 3 * STEP);
+		// this.GL.disableVertexAttribArray(colorLocation);
 
 		//draw the triangles as set by the position buffers
 		this.GL.drawArrays(this.GL.TRIANGLES,0,3);
