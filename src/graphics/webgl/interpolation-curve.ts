29a1fea19c62a132a815a6854ce435e83e78df47
Nikhil Verma <nikhilnxvverma1@aol.com>
2017-03-31 23:15:17 -0400

    Added animation effect class as behavior

---
+++ src/graphics/webgl/interpolation-curve.ts
@@ -0,0 +1,39 @@
+/** Governs the speed of change of a value across time*/
+export interface InterpolationCurve{
+	/**
+	 * Curve that gives the speed of change of a value at a given fraction of progress.
+	 * @param timeFraction value between 0 and 1 that denotes the progress of the animation so far
+	 * @return value between 0 and 1 (overshoots allowed) that denotes the value at that time
+	 */
+	interpolationAt(timeFraction:number):number;
+}
+
+export class Linear implements InterpolationCurve{
+	interpolationAt(timeFraction:number):number{
+		return timeFraction;
+	}
+}
+
+export class EaseInQuadratic implements InterpolationCurve{
+	interpolationAt(timeFraction:number):number{
+		return timeFraction*timeFraction*timeFraction;//quadratic
+	}
+}
+
+export class EaseOutQuadratic implements InterpolationCurve{
+	interpolationAt(timeFraction:number):number{
+		return 1-timeFraction*timeFraction*timeFraction;//quadratic
+	}
+}
+
+export class EaseInCubic implements InterpolationCurve{
+	interpolationAt(timeFraction:number):number{
+		return timeFraction*timeFraction*timeFraction;//cubic
+	}
+}
+
+export class EaseOutCubic implements InterpolationCurve{
+	interpolationAt(timeFraction:number):number{
+		return 1-timeFraction*timeFraction*timeFraction;//cubic
+	}
+}
